cb53690eb27381d8ebabf739a9ec782997b75fe7
Johannes Link <business@johanneslink.net>
2017-06-26 11:09:34 +0200

    Added java fizz buzz properties

--- pbt-java/src/test/java/pbt/fizzbuzz/FizzBuzzProperties.java
+++ pbt-java/src/test/java/pbt/fizzbuzz/FizzBuzzProperties.java
@@ -1,12 +1,45 @@
 package pbt.fizzbuzz;
 
 import net.jqwik.api.*;
+import net.jqwik.properties.*;
 
 class FizzBuzzProperties {
 
 	@Property
-	boolean failForAStart() {
-		return false;
+	boolean divisibleBy3ContainsFizz(@ForAll("divisibleBy3") int anInt) {
+		return fizzBuzz(anInt).contains("Fizz");
+	}
+
+	@Generate
+	Arbitrary<Integer> divisibleBy3() {
+		return Generator.integer(1, 100).filter(i -> i % 3 == 0);
+	}
+
+	@Property
+	boolean divisibleBy5ContainsBuzz(@ForAll("divisibleBy5") int anInt) {
+		return fizzBuzz(anInt).contains("Buzz");
+	}
+
+	@Generate
+	Arbitrary<Integer> divisibleBy5() {
+		return Generator.integer(1, 100).filter(i -> i % 5 == 0);
+	}
+
+	@Property
+	boolean undivisiblesReturnThemselves(@ForAll("notDivisible") int anInt) {
+		return fizzBuzz(anInt).equals(Integer.toString(anInt));
+	}
+
+	@Generate
+	Arbitrary<Integer> notDivisible() {
+		return Generator.integer(1, 100) //
+				.filter(i -> i % 3 != 0 && i % 5 != 0);
+	}
+
+	private String fizzBuzz(int anInt) {
+		if (anInt % 3 == 0)
+			return "Fizz";
+		return Integer.toString(anInt);
 	}
 
 }
