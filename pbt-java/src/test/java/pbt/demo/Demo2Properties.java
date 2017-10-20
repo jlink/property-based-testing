ca2e75afd87d78cd17fdf75cfc1ed475a9c3f6f9
Johannes Link <business@johanneslink.net>
2017-10-20 17:45:02 +0200

    Update to jqwik 0.5.4 and add Demo2Properties

--- pbt-java/src/test/java/pbt/demo/Demo2Properties.java
+++ pbt-java/src/test/java/pbt/demo/Demo2Properties.java
@@ -1,14 +1,43 @@
 package pbt.demo;
 
 import net.jqwik.api.*;
+import net.jqwik.properties.*;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.data.Percentage;
+
+import static org.assertj.core.data.Percentage.*;
 
 class Demo2Properties {
 
-	@Property
+	@Property(shrinking = ShrinkingMode.OFF)
 	void squareOfRootIsOriginalValue(@ForAll double aNumber) {
 		double sqrt = Math.sqrt(aNumber);
-		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, Percentage.withPercentage(10));
+		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(10));
 	}
+
+	@Property(shrinking = ShrinkingMode.OFF)
+	void squareOfRootIsOriginalValue1(
+			@ForAll @DoubleRange(min = 0, max = Double.MAX_VALUE) double aNumber
+	) {
+		double sqrt = Math.sqrt(aNumber);
+		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
+	}
+
+	@Property(shrinking = ShrinkingMode.OFF)
+	void squareOfRootIsOriginalValue2(@ForAll("positiveDouble") double aNumber) {
+		double sqrt = Math.sqrt(aNumber);
+		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
+	}
+
+	@Generate
+	Arbitrary<Double> positiveDouble() {
+		return Arbitraries.doubles().filter(aDouble -> aDouble > 0);
+	}
+
+	@Property(shrinking = ShrinkingMode.OFF)
+	void squareOfRootIsOriginalValue3(@ForAll double aNumber) {
+		Assume.that(aNumber > 0);
+		double sqrt = Math.sqrt(aNumber);
+		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
+	}
+
 }
