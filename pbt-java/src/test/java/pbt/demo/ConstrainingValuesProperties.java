acf64082b601e7a0709f5d18881f937c5717bcae
Johannes Link <business@johanneslink.net>
2017-10-21 16:50:37 +0200

    Update to jqwik 0.5.5

--- pbt-java/src/test/java/pbt/demo/ConstrainingValuesProperties.java
+++ pbt-java/src/test/java/pbt/demo/ConstrainingValuesProperties.java
@@ -6,24 +6,25 @@ import org.assertj.core.api.Assertions;
 
 import static org.assertj.core.data.Percentage.*;
 
-class Demo2Properties {
+class ConstrainingValuesProperties {
 
-	@Property(shrinking = ShrinkingMode.OFF)
+	// Expected to fail since there is no sqrt of negative numbers
+	@Property
 	void squareOfRootIsOriginalValue(@ForAll double aNumber) {
 		double sqrt = Math.sqrt(aNumber);
 		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(10));
 	}
 
-	@Property(shrinking = ShrinkingMode.OFF)
-	void squareOfRootIsOriginalValue1(
+	@Property
+	void squareOfRootIsOriginalValue_1(
 			@ForAll @DoubleRange(min = 0, max = Double.MAX_VALUE) double aNumber
 	) {
 		double sqrt = Math.sqrt(aNumber);
 		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
 	}
 
-	@Property(shrinking = ShrinkingMode.OFF)
-	void squareOfRootIsOriginalValue2(@ForAll("positiveDouble") double aNumber) {
+	@Property
+	void squareOfRootIsOriginalValue_2(@ForAll("positiveDouble") double aNumber) {
 		double sqrt = Math.sqrt(aNumber);
 		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
 	}
@@ -33,8 +34,8 @@ class Demo2Properties {
 		return Arbitraries.doubles().filter(aDouble -> aDouble > 0);
 	}
 
-	@Property(shrinking = ShrinkingMode.OFF)
-	void squareOfRootIsOriginalValue3(@ForAll double aNumber) {
+	@Property
+	void squareOfRootIsOriginalValue_3(@ForAll double aNumber) {
 		Assume.that(aNumber > 0);
 		double sqrt = Math.sqrt(aNumber);
 		Assertions.assertThat(sqrt * sqrt).isCloseTo(aNumber, withPercentage(1));
