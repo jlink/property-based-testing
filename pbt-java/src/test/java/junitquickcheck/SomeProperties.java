3272393936896476f8e7f2b77fa6daa627e17685
Johannes Link <business@johanneslink.net>
2018-04-16 17:24:36 +0200

    Added shrinking examples for quicktheories and junit-quickcheck

--- pbt-java/src/test/java/junitquickcheck/SomeProperties.java
+++ pbt-java/src/test/java/junitquickcheck/SomeProperties.java
@@ -1,7 +1,9 @@
 package junitquickcheck;
 
 import com.pholser.junit.quickcheck.*;
-import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.generator.*;
+import com.pholser.junit.quickcheck.generator.java.lang.IntegerGenerator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.*;
 import org.junit.runner.RunWith;
@@ -16,11 +18,30 @@ public class SomeProperties {
 	}
 
 	@Property()
-	public void shouldShrinkTo11ButDoesNot(
+	public void shouldShrinkTo101ButDoesNot(
 			@When(seed = -4386629332000517955L) //
 			@InRange(minInt = 1, maxInt = 1000) int i
 	) {
-		Assume.assumeTrue(i > 10);
+		Assume.assumeTrue(i > 100);
 		Assert.assertTrue(i % 2 == 0);
+	}
+
+
+	@Property()
+	public void shouldShrinkTo101String(@From(NumericString.class) String iString) {
+		int i = Integer.parseInt(iString);
+		Assert.assertTrue(i % 2 == 0);
+	}
+
+	public static class NumericString extends Generator<String> {
+
+		public NumericString() {
+			super(String.class);
+		}
+
+		@Override
+		public String generate(SourceOfRandomness random, GenerationStatus status) {
+			return String.valueOf(random.nextInt(100, 1000));
+		}
 	}
 }
--- pbt-java/src/test/java/quicktheories/SomeTests.java
+++ pbt-java/src/test/java/quicktheories/SomeTests.java
@@ -1,18 +1,26 @@
 package quicktheories;
 
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Test;
 
 import static org.quicktheories.QuickTheory.*;
 import static org.quicktheories.generators.SourceDSL.*;
 
-public class SomeTests {
+class SomeTests {
 
 	@Test
-	public void addingTwoPositiveIntegersAlwaysGivesAPositiveInteger() {
+	void shouldShrinkTo101() {
 		qt() //
-				.forAll(integers().allPositive() //
-						, integers().allPositive()) //
-				.check((i, j) -> i + j > 0);
+				.forAll(integers().between(100, 1000)) //
+				.check(i -> i % 2 == 0);
+	}
+
+	@Test
+	void shouldShrinkTo101String() {
+		qt() //
+				.forAll(integers().between(100, 1000).map((Function<Integer, String>) String::valueOf)) //
+				.check(s -> Integer.parseInt(s) % 2 == 0);
 	}
 
 }
