6592c8dbf615af77255deee803befe99730ddf28
Johannes Link <business@johanneslink.net>
2018-04-18 15:14:14 +0200

    Added shrinking example

--- pbt-java/src/test/java/pbt/shrinking/SimpleShrinkingExamples.java
+++ pbt-java/src/test/java/pbt/shrinking/SimpleShrinkingExamples.java
@@ -1,7 +1,7 @@
 package pbt.shrinking;
 
 import net.jqwik.api.*;
-import net.jqwik.api.constraints.*;
+import net.jqwik.api.constraints.Positive;
 
 class SimpleShrinkingExamples {
 
@@ -11,4 +11,15 @@ class SimpleShrinkingExamples {
 		int square = anInt * anInt;
 		return Math.sqrt(square) == anInt;
 	}
+
+	@Property
+	boolean shouldShrinkTo101(@ForAll("numberStrings") String aNumberString) {
+		return Integer.parseInt(aNumberString) % 2 == 0;
+	}
+
+	@Provide
+	Arbitrary<String> numberStrings() {
+		return Arbitraries.integers().between(100, 1000).map(String::valueOf);
+	}
+
 }
