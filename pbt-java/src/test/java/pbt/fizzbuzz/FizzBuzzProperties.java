cc00702108a02e66551456d32145abf2a6d8da9e
Johannes Link <business@johanneslink.net>
2018-07-11 08:46:06 +0200

    Add labels to FizzBuzzProperties

--- pbt-java/src/test/java/pbt/fizzbuzz/FizzBuzzProperties.java
+++ pbt-java/src/test/java/pbt/fizzbuzz/FizzBuzzProperties.java
@@ -3,9 +3,11 @@ package pbt.fizzbuzz;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
 
+@Label("Calling fizzBuzz with...")
 class FizzBuzzProperties {
 
 	@Property
+	@Label("a number divisible by 3 contains 'Fizz'")
 	boolean divisibleBy3ContainsFizz(@ForAll("divisibleBy3") int anInt) {
 		return fizzBuzz(anInt).contains("Fizz");
 	}
@@ -15,23 +17,25 @@ class FizzBuzzProperties {
 		return Arbitraries.integers().between(1, 33).map(i -> i * 3);
 	}
 
-	@Example
+	//@Example
 	boolean divisibleBy3AndNot5ReturnsFizz() {
 		return fizzBuzz(3).equals("Fizz");
 	}
 
 	@Property
+	@Label("a number divisible by 5 contains 'Buzz'")
 	boolean divisibleBy5ContainsBuzz(@ForAll @IntRange(min = 1, max = 100) int anInt) {
 		Assume.that(anInt % 5 == 0);
 		return fizzBuzz(anInt).contains("Buzz");
 	}
 
-	@Example
+	//@Example
 	boolean divisibleBy5AndNot3ReturnsBuzz() {
 		return fizzBuzz(5).equals("Buzz");
 	}
 
 	@Property
+	@Label("a number not divisible by 3 or 5 returns the number itself")
 	boolean indivisiblesReturnThemselves(@ForAll("notDivisible") int anInt) {
 		return fizzBuzz(anInt).equals(Integer.toString(anInt));
 	}
@@ -43,6 +47,7 @@ class FizzBuzzProperties {
 	}
 
 	@Property(maxDiscardRatio = 20)
+	@Label("a number divisible by 3 and 5 returns 'FizzBuzz'")
 	boolean divisibleBy3and5ReturnFizzBuzz(@ForAll @IntRange(min = 1, max = 100) int anInt) {
 		Assume.that(anInt % 3 == 0);
 		Assume.that(anInt % 5 == 0);
