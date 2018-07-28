6fefffb653cf2bdbe3a4a0cddcaf6497f12fb206
Johannes Link <business@johanneslink.net>
2018-07-28 12:42:11 +0200

    Added invariant to circular buffer properties

--- pbt-java/src/test/java/pbt/stateful/circularbuffer/CircularBufferProperties.java
+++ pbt-java/src/test/java/pbt/stateful/circularbuffer/CircularBufferProperties.java
@@ -5,11 +5,21 @@ import pbt.stateful.circularbuffer.CircularBufferActions.*;
 import net.jqwik.api.*;
 import net.jqwik.api.stateful.*;
 
+import static org.assertj.core.api.Assertions.*;
+
 class CircularBufferProperties {
 
 	@Property //(reporting = Reporting.GENERATED)
 	void checkSequentialStateMachine(@ForAll("sequences") ActionSequence<Model> sequence) {
-		sequence.run(new Model());
+
+		Invariant<Model> sizeMustNotBeNegative = model ->
+				assertThat(model.buffer.size())
+						.as("Size must not be negative")
+						.isGreaterThanOrEqualTo(0);
+
+		sequence
+				.withInvariant(sizeMustNotBeNegative)
+				.run(new Model());
 	}
 
 	@Provide
