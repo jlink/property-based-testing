31412f57a292b3453b9ef770a6a976cf80f6e3f9
Johannes Link <business@johanneslink.net>
2018-10-22 12:19:43 +0200

    small change in shrinking example

--- pbt-java/src/test/java/pbt/shrinking/SimpleShrinkingExamples.java
+++ pbt-java/src/test/java/pbt/shrinking/SimpleShrinkingExamples.java
@@ -5,21 +5,22 @@ import net.jqwik.api.constraints.Positive;
 
 class SimpleShrinkingExamples {
 
-	@Property(shrinking = ShrinkingMode.FULL) @Report(Reporting.GENERATED)
+	@Property(shrinking = ShrinkingMode.FULL)
+	@Report(Reporting.FALSIFIED)
 	boolean rootOfSquareShouldBeOriginalValue(@Positive @ForAll int anInt) {
 		Assume.that(anInt != Integer.MAX_VALUE);
 		int square = anInt * anInt;
 		return Math.sqrt(square) == anInt;
 	}
 
-	@Property(seed = "-2", shrinking = ShrinkingMode.FULL, generation = GenerationMode.RANDOMIZED)
+	@Property(shrinking = ShrinkingMode.FULL)
 	boolean shouldShrinkTo101(@ForAll("numberStrings") String aNumberString) {
 		return Integer.parseInt(aNumberString) % 2 == 0;
 	}
 
 	@Provide
 	Arbitrary<String> numberStrings() {
-		return Arbitraries.integers().between(100, 1000).map(String::valueOf);
+		return Arbitraries.integers().between(100, 10000).map(String::valueOf);
 	}
 
 }
