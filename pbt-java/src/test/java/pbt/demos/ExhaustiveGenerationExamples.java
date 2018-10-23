eeda3c8418ab00d3d04027c47341eb34964554f9
Johannes Link <business@johanneslink.net>
2018-10-23 16:01:37 +0200

    Added examples for exhaustive generation

--- pbt-java/src/test/java/pbt/demos/ExhaustiveGenerationExamples.java
+++ pbt-java/src/test/java/pbt/demos/ExhaustiveGenerationExamples.java
@@ -1,11 +1,13 @@
 package pbt.demos;
 
+import java.util.*;
+
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.*;
 
 class ExhaustiveGenerationExamples {
 
-	@Property(generation = GenerationMode.EXHAUSTIVE)
+	@Property
 	void allChessSquares(
 			@ForAll @CharRange(from = 'a', to = 'h') char column,
 			@ForAll @CharRange(from = '1', to = '8')char row
@@ -14,4 +16,13 @@ class ExhaustiveGenerationExamples {
 		System.out.println(square);
 	}
 
+	// Will generate 532441 different matrices
+	@Property(generation = GenerationMode.EXHAUSTIVE)
+	void matrix3times3(@ForAll @Size(3) List< @Size(3) List<@IntRange(min = 1, max = 9) Integer>> matrix) {
+		System.out.println(format(matrix));
+	}
+
+	private String format(List<List<Integer>> matrix) {
+		return String.format("%s%n%s%n%s%n", matrix.get(0), matrix.get(1), matrix.get(2));
+	}
 }
