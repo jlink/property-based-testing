b4dd6ae829407148e9b5c9719223801a48d06b06
Johannes Link <business@johanneslink.net>
2018-10-29 11:15:23 +0100

    Simplified commutativity example

--- pbt-java/src/test/java/pbt/patterns/Commutativity.java
+++ pbt-java/src/test/java/pbt/patterns/Commutativity.java
@@ -1,6 +1,8 @@
 package pbt.patterns;
 
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
 import org.assertj.core.api.*;
 
 import java.util.*;
@@ -9,26 +11,18 @@ import java.util.stream.*;
 class Commutativity {
 
 	@Property @Report(Reporting.GENERATED)
-	void sortingAndFilteringAreCommutative(@ForAll("names") List<String> listOfNames) {
-		List<String> filteredThenSorted = listOfNames.stream() //
-				.filter(name -> !name.contains("a")) //
-				.sorted() //
+	void sortingAndFilteringAreCommutative(@ForAll List<@AlphaChars String> listOfNames) {
+		List<String> filteredThenSorted = listOfNames.stream()
+				.filter(name -> !name.toLowerCase().contains("a"))
+				.sorted()
 				.collect(Collectors.toList());
 
-		List<String> sortedThenFiltered = listOfNames.stream() //
-				.sorted() //
-				.filter(name -> !name.contains("a")) //
+		List<String> sortedThenFiltered = listOfNames.stream()
+				.sorted()
+				.filter(name -> !name.toLowerCase().contains("a"))
 				.collect(Collectors.toList());
 
 		Assertions.assertThat(filteredThenSorted).isEqualTo(sortedThenFiltered);
-	}
-
-	@Provide
-	Arbitrary<List<String>> names() {
-		Arbitrary<String> name = Arbitraries.strings() //
-				.withCharRange('a', 'z') //
-				.ofMinLength(3).ofMaxLength(40);
-		return name.list().ofMinSize(0).ofMaxSize(30);
 	}
 
 }
