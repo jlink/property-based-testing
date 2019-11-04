e63e1a54c5fb793633a88607ebaa2f965d22c3e4
Johannes Link <business@johanneslink.net>
2019-11-04 13:02:35 +0100

    co2 budget: Changed properties to reflect changed presentation story

--- pbt-java/src/test/java/pbt/co2budget/CO2Budget.java
+++ pbt-java/src/test/java/pbt/co2budget/CO2Budget.java
@@ -1,12 +1,14 @@
 package pbt.co2budget;
 
 public class CO2Budget {
-	public static int remainingYears(int initialBudget, int startingAnnualEmission, int annualChange) {
+	public static int remainingYears(
+			int initialBudget, int startingAnnualEmission, int annualChange
+	) {
 		int years = 0;
 		int remainingBudget = initialBudget;
 		while (remainingBudget > 0) {
 			if (startingAnnualEmission <= 0) {
-				return Integer.MAX_VALUE;
+				return 1000; // or any non negative value
 			}
 			years++;
 			remainingBudget = remainingBudget - startingAnnualEmission;
--- pbt-java/src/test/java/pbt/co2budget/CO2BudgetSpec.java
+++ pbt-java/src/test/java/pbt/co2budget/CO2BudgetSpec.java
@@ -16,28 +16,20 @@ class CO2BudgetSpec {
 	}
 
 	@Group
-	class NoAnnualChange {
+	class WithoutAnnualChange {
 		@Example
-		void annualEmissionIsExactMultipleOfInitialBudget() {
-			assertEquals(10, CO2Budget.remainingYears(100, 10, 0));
+		void budgetIsUsedUp() {
+			assertEquals(11, CO2Budget.remainingYears(105, 10, 0));
 		}
 
 		@Property
-		void annualEmissionIsExactMultipleOfInitialBudget(
-				@ForAll @IntRange(min = 1, max = 1000) int multiple,
-				@ForAll @IntRange(min = 1, max = Integer.MAX_VALUE / 1000) int startingAnnual
+		void budgetIsUsedUp(
+				@ForAll @IntRange(min = 1, max = 1000) int remainingYears,
+				@ForAll @IntRange(min = 2, max = Integer.MAX_VALUE/1000) int startingAnnual,
+				@ForAll @IntRange(min = -1, max = 0) int delta
 		) {
-			int initialBudget = startingAnnual * multiple;
-			assertEquals(multiple, CO2Budget.remainingYears(initialBudget, startingAnnual, 0));
-		}
-
-		@Property
-		void annualEmissionIsNotExactMultipleOfInitialBudget(
-				@ForAll @IntRange(min = 0, max = 1000) int multiple,
-				@ForAll @IntRange(min = 1, max = Integer.MAX_VALUE / 1000) int startingAnnual
-		) {
-			int initialBudget = startingAnnual * multiple + 1;
-			assertEquals(multiple + 1, CO2Budget.remainingYears(initialBudget, startingAnnual, 0));
+			int initialBudget = startingAnnual * remainingYears + delta;
+			assertEquals(remainingYears, CO2Budget.remainingYears(initialBudget, startingAnnual, 0));
 		}
 
 		@Property
@@ -47,6 +39,21 @@ class CO2BudgetSpec {
 		) {
 			int years = CO2Budget.remainingYears(initialBudget, startingAnnual, 0);
 			return years >= 0;
+		}
+	}
+
+	@Group
+	@Disabled
+	class WithAnnualChange {
+		@Example
+		void budgetIsUsedUpDespiteDecrease() {
+			assertEquals(8, CO2Budget.remainingYears(100, 20, -2));
+			assertEquals(5, CO2Budget.remainingYears(170, 42, -4));
+		}
+
+		@Example
+		void budgetIsUsedUpWithIncrease() {
+			assertEquals(5, CO2Budget.remainingYears(100, 20, 2));
 		}
 	}
 
