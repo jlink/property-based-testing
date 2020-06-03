b4b5828797d8290f0dcbc22414336d858a782f83
Johannes Link <business@johanneslink.net>
2020-06-03 10:24:18 +0200

    Added a few statistics to Spotify example

--- pbt-java/src/test/java/pbt/spotify/SpotifyProperties.java
+++ pbt-java/src/test/java/pbt/spotify/SpotifyProperties.java
@@ -3,12 +3,17 @@ package pbt.spotify;
 import java.util.*;
 
 import net.jqwik.api.*;
+import net.jqwik.api.statistics.*;
 
 class SpotifyProperties {
 
-	@Property(tries = 10)
+	@Property(tries = 1000)
+	@StatisticsReport(format = NumberRangeHistogram.class)
 	void checkArbitraries(@ForAll("spotify")Tuple.Tuple3<Set<Artist>, Set<Album>, Set<Song>> spotify) {
-		System.out.println(spotify);
+		Statistics.label("artists").collect(spotify.get1().size());
+		Statistics.label("albums").collect(spotify.get2().size());
+		Statistics.label("songs").collect(spotify.get3().size());
+		// System.out.println(spotify);
 	}
 
 	@Provide
